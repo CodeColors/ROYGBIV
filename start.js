90caf88828ebe62e031057343857a40b5fbd8e75
Ionică Bizău <bizauionica@gmail.com>
2020-08-10 07:18:04 +0300

    use sync version

--- start.js
+++ start.js
@@ -7,5 +7,5 @@ var DIR = "./data/"
 for (var i in colors) {
     var text = Generator.start(colors[i].length);
 
-    fs.writeFile(DIR + colors[i].language + colors[i].extension, text);
+    fs.writeFileSync(DIR + colors[i].language + colors[i].extension, text);
 }
